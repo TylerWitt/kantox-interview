764b2d69bfe1852e2b9eb073636071290e9fd85b
Tyler Witt <Ty.Mo.Witt@gmail.com>
2025-05-05 09:31:33 -0500

    Init new elixir app
    This is the result of `mix new`

---
+++ lib/basket.ex
@@ -0,0 +1,18 @@
+defmodule Basket do
+  @moduledoc """
+  Documentation for `Basket`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Basket.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
